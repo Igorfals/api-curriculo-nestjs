6e1ce04d85e4a85ab0836b8349fa2dc55d28ed51
igorfals <igorfeals@gmail.com>
2025-06-01 14:52:13 -0300

    feat(user): implemente create

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  declare email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  declare password: string;
+
+  @IsString()
+  declare name: string;
+
+  @IsOptional()
+  declare id?: number;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const creteUser = await this.prisma.user.create({ data });
+
+    return {
+      ...creteUser,
+      password: undefined,
+    };
   }
 
   findAll() {
