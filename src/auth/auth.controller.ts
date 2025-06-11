1f9101e4bbc2581033ad461b8bd5233590c5d6bb
igorfals <igorfeals@gmail.com>
2025-06-11 02:33:57 -0300

    feat(auth): implemente jwt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,12 @@ import {
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/auth-request';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public-decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
